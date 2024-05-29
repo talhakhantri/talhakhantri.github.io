caf3e03553d4e9f07b2e6fc75e029cbe14e51688
talhakhantri <talha.khan@tri.global>
2024-05-29 09:13:12 -0700

    fixing height

--- script.js
+++ script.js
@@ -135,5 +135,12 @@ eventer(messageEvent, function (e) {
 
 
 
+window.onload = function() {
+    var height = document.body.scrollHeight;
+    window.parent.postMessage({ frameHeight: height }, '*');
+};
+
+
+
 
 
